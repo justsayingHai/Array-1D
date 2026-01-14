8c90cdc4b63c63c27fd7be9b4c72eec212cfc386
justsayingHai <builehoanghai@gmail.com>
2026-01-14 14:40:21 -0600

    Add files via upload

---
+++ Secret PIN Attempts.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int size = 5;
+    int pin[size];
+
+    for (int index = 0; index < size; index++)
+    {
+        cin >> pin[index];
+    }
+
+    int third_attempt = pin[2];
+}
+
+
